e98027f2613dfcd9e1829358932f751810357d2d
xh114 <xh114@duke.edu>
2021-09-03 03:55:52 -0400

    m

--- 016_code2/code2.c
+++ 016_code2/code2.c
@@ -1,17 +1,21 @@
 
 int printTriangle(int size) {
-  //start with starCount being 0
-
+       int starCount =0;	//start with starCount being 0
+       int i=0;
+       for(i=0;i<size;i++){
   //count from 0 (inclusive) to size (exclusive), for each number i that you count
-
+          int j=0;
+	  for(j=0;j<=i;j++){
      //count from 0 (inclusive) to i (inclusive), for each number j that you count
-
+                printf ("*");
         //print a "*"
-
+                starCount=starCount+1;
+	  }
         //increment starCount
-
+          printf ("\n");
+       }    
      //when you finish counting on j, 
-
+       return starCount;
      //print a newline ("\n")
 
   //when you finish counting on i, 
@@ -29,7 +33,9 @@ int main(void) {
   //now print "Here is a triangle with height 7\n"
 
   //then call printTriangle, passing in 7, and assign the result to numStars
-
+  printf("Here is a triangle with height 7\n");
+  numStars = printTriangle(7);
+  printf("That triangle had %d total stars\n",numStars);
   //finally, print "That triangle had %d total stars\n", such that the %d 
   //prints the value of numStars
 
