1db4ad0010dd2288e85318d02a49db9fc71fb7ff
xh114 <xh114@duke.edu>
2021-09-26 13:27:33 -0400

    m

--- 028_tests_power/test-power.c
+++ 028_tests_power/test-power.c
@@ -1,10 +1,12 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 unsigned power(unsigned x, unsigned y);
 
 int run_check(unsigned x, unsigned y, unsigned expected_ans) {
-  if (power(x, y) != expected_ans) {
+  unsigned ans = power(x, y);
+  if (ans != expected_ans) {
+    printf("wrong!! x=%u y=%u ans=%u expected=%u \n", x, y, ans, expected_ans);
     exit(EXIT_FAILURE);
   }
   else {
@@ -13,18 +15,10 @@ int run_check(unsigned x, unsigned y, unsigned expected_ans) {
 }
 
 int main(void) {
-  //  if (run_check(0, 0, 1) || run_check(0, 1, 0) || run_check(1, 0, 1) ||
-  //  run_check(2, 2, 4) || run_check(2, 5, 32) || run_check(3, 10, 59049)||
-  //    run_check(80,0,1) || run_check() {
-  //  return EXIT_FAILURE;
-  //}
-  for (unsigned x = 0; x <= 100; x++) {
-    for (unsigned y = 0; y <= 100; y++) {
-      if (x == 0) {
-        run_check(x, y, 1);
-      }
-      run_check(x, y, x ^ y);
-    }
+  if (run_check(0, 0, 1) || run_check(0, 1, 0) || run_check(1, 0, 1) ||
+      run_check(2, 2, 4) || run_check(2, 5, 32) || run_check(3, 10, 59049) ||
+      run_check(80, 0, 1) || run_check(63240, 2, 3999297600)) {
+    return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
 }
