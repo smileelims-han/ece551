bc5e84dedd02371fa382c47533ffd4b449bd9ae3
xh114 <xh114@duke.edu>
2021-10-05 17:31:11 -0400

    m

---
+++ 943_eval1_late/pandemic.c
@@ -0,0 +1,76 @@
+#include "pandemic.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+country_t parseLine(char * line) {
+  if (line == NULL) {
+    fprintf(stderr, "Input is empty.");
+    exit(EXIT_FAILURE);
+  }
+  /* check if *line is empty string */
+  /* if not empty string,find the first comma */
+
+  country_t ans;
+  const char *calc,
+      *ref; /* set up two pointer, one for calculation the length, one for reference the start. */
+  size_t name_length;
+  calc = line;
+  ref = line;
+
+  /* check the location of comma, at the same time check if there has incorrect null terminal inside name */
+  for (int i = 0; *calc != ','; i++) {
+    if (*calc == '\0') {
+      fprintf(stderr, "There is incorrect type in country's name.");
+      exit(EXIT_FAILURE);
+    }
+    else {
+      calc++;
+    }
+  }
+  name_length = calc - ref;
+
+  /* copy the chars in line before comma into ans.name */
+  strncpy(ans.name, line, name_length);
+  ans.name[name_length] = '\0';
+
+  /* Then check the chars after comma, are they all numbers */
+  calc++;
+  ref = calc;
+
+  for (int j = 0; *calc != '\0' && *calc != ','; j++) {
+    if (*calc == ',') {
+      fprintf(stderr, "There is a comma inside pupolation.");
+      exit(EXIT_FAILURE);
+    }
+    /* check if there is another comma just in case. */
+    if (*calc > '9' || *calc < '0') {
+      fprintf(stderr, "The population should be all numbers.");
+      exit(EXIT_FAILURE);
+    }
+    /* check if all the chars between comma and null terminal are numbers. */
+    else {
+      ans.population = ans.population * 10 + (uint64_t)(*ref - '0');
+      ref++;
+    }
+    /* copy the chars in population in line into the pop */
+    calc++;
+  }
+  return ans;
+}
+
+/* void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
+  //WRITE ME
+}
+
+void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) {
+  //WRITE ME
+}
+
+void printCountryWithMax(country_t * countries,
+                         size_t n_countries,
+                         unsigned ** data,
+                         size_t n_days) {
+  //WRITE ME
+  }*/
