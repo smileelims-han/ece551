84670f9f390fbb82c8c442113b33403216d68dbe
xh114 <xh114@duke.edu>
2021-10-06 01:57:01 -0400

    m

--- 943_eval1_late/pandemic.c
+++ 943_eval1_late/pandemic.c
@@ -6,21 +6,25 @@
 #include <string.h>
 
 country_t parseLine(char * line) {
+  /* check if is empty string */
   if (line == NULL) {
     fprintf(stderr, "Input is empty.");
     exit(EXIT_FAILURE);
   }
-  /* check if *line is empty string */
-  /* if not empty string,find the first comma */
 
+  /* find the first comma */
   country_t ans;
-  char *calc,
-      *ref; /* set up two pointer, one for calculation the length, one for reference the start. */
   size_t name_length;
+  char *calc, *ref;
   calc = line;
   ref = line;
+  /* set up two pointers, one will move
+  in the array to check the desired symbol;
+  one will set at the begining for reference.
+  The difference between the two pointers is
+  the length of name or population.*/
 
-  /* check the location of comma, at the same time check if there has incorrect null terminal inside name */
+  /* Get the location of comma */
   for (int i = 0; *calc != ','; i++) {
     if (*calc == '\0') {
       fprintf(stderr, "There is incorrect type in country's name.");
@@ -32,38 +36,51 @@ country_t parseLine(char * line) {
   }
   name_length = calc - ref;
 
-  /* copy the chars in line before comma into ans.name */
+  /* copy the chars in sting before comma into ans.name */
   strncpy(ans.name, line, name_length);
   ans.name[name_length] = '\0';
 
-  /* Then check the chars after comma, are they all numbers */
+  /* Then start work for then population */
+  /* move the pointers to the first chars after comma */
   calc++;
   ref = calc;
-  ans.population = 0ULL;
+  ans.population = 0ul;
   size_t pop_length;
+
+  /* moving the pointer to the end of numbers and
+  calculate the length of numbers */
   for (int j = 0; *calc != '\0'; j++) {
     calc++;
   }
   pop_length = calc - ref;
-  char * pop[pop_length];
 
+  /* copy the parts after comma into a new string */
+  char * pop[pop_length];
   for (int k = 0; *ref != *calc; k++) {
     pop[k] = ref;
     ref++;
   }
 
+  /* using atoi to transfer the chars into numbers */
   ans.population = atoi(*pop);
   if (ans.population == 0) {
     fprintf(stderr, "The input format is incorrect.\n");
     exit(EXIT_FAILURE);
   }
+  /* return FAILURE when the first symbol
+     (excluded space) is not number */
   if (ans.population < 0) {
     ans.population = (uint64_t)ans.population;
   }
+  /* incase there is negative number, transfer
+     it into uint64_t */
+
   return ans;
 }
 
 void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
+  /* return error if the input data is empty
+     or the inputs days less than 7 days (so we cannot calculate)*/
   if (data == NULL) {
     fprintf(stderr, "There is an emtpy input.\n");
     exit(EXIT_FAILURE);
@@ -72,9 +89,13 @@ void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
     fprintf(stderr, "There is not enough days as input.\n");
     exit(EXIT_FAILURE);
   }
+
   size_t avg_length = n_days - 6;
   unsigned * ptr = data;
   unsigned * ref = data;
+
+  /* calculate the sum of 7 days(from the start date),
+     then divided into 7*/
   for (size_t i = 0; i < avg_length; i++) {
     double sum = 0;
     for (size_t j = 0; j < 7; j++) {
@@ -82,13 +103,15 @@ void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
       ptr++;
     }
     avg[i] = sum / 7.0;
-    ptr = ref + i + 1;
+    ptr = ref + i + 1; /*every time after calculate 7 days,
+	          	the pointer goes back to the one next to index i*/
   }
 }
 
 void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) {
+  /* check if the input data is empty*/
   if (data == NULL) {
-    fprintf(stderr, "There is an emtpy input.\n");
+    fprintf(stderr, "There is an empty input.\n");
     exit(EXIT_FAILURE);
   }
 
@@ -99,11 +122,15 @@ void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) 
   }
 }
 
+/* function findMax for step 4 */
 size_t findMax(unsigned * data, size_t n_days) {
   size_t maxnum_loc = 0;
   unsigned maxnum;
   maxnum = *data;
 
+  /* compare the max with each number and
+  update the max, stored the index of maxnumber 
+  in the giving string */
   for (size_t i = 0; i < n_days; i++) {
     if (maxnum < *(data + i)) {
       maxnum = *(data + i);
@@ -117,17 +144,22 @@ void printCountryWithMax(country_t * countries,
                          size_t n_countries,
                          unsigned ** data,
                          size_t n_days) {
+  /*check if the inputs data are empty */
   if (countries == NULL || data == NULL) {
     fprintf(stderr, "There is an emtpy input.\n");
     exit(EXIT_FAILURE);
   }
 
+  /*first, find the maxnumber of each country in
+  given days, stored it into a new array which ordered
+  based on the country sequence in the giving array*/
   unsigned maxPer_con[n_countries];
   for (size_t i = 0; i < n_countries; i++) {
     size_t max_loc = findMax(*(data + i), n_days);
     maxPer_con[i] = *(data[i] + max_loc);
   }
 
+  /*Then, find the maxnumber of all countries */
   size_t max_country = findMax(maxPer_con, n_countries);
   printf("%s has the most daily cases with %u\n",
          (countries + max_country)->name,
