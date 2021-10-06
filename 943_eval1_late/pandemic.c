4a40997087dd3ef466245635be85856b467bcfc9
xh114 <xh114@duke.edu>
2021-10-06 12:07:57 -0400

    m

--- 943_eval1_late/pandemic.c
+++ 943_eval1_late/pandemic.c
@@ -68,7 +68,7 @@ country_t parseLine(char * line) {
   /* return FAILURE when the first symbol
      (excluded space) is not number */
   if (ans.population < 0) {
-    ans.population = (uint64_t)ans.population;
+    ans.population = 0 - ans.population;
   }
   /* incase there is negative number, transfer
      it into uint64_t */
