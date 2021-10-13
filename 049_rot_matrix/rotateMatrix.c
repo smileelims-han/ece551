c228d9255ec24faf35cc6498d929d4519ac298f6
xh114 <xh114@duke.edu>
2021-10-12 20:20:53 -0400

    m

---
+++ 049_rot_matrix/rotateMatrix.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv) {
+  if (argc != 2) {
+    fprintf(stderr, "The comman line argument is not correct.");
+    return EXIT_FAILURE;
+  }
+  FILE * f = fopen(argv[1], "r");
+  if (f == NULL) {
+    fprintf(stderr, "The input file is empty.");
+    return EXIT_FAILURE;
+  }
+  char input[10][10];
+  int c;
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 11; j++) {
+      c = fgetc(f);
+      if (j < 10 && c == '\n') {
+        fprintf(stderr, "The number of chars in row less than 10.");
+        return EXIT_FAILURE;
+      }
+      if (j < 10 && c != '\n') {
+        input[i][j] = c;
+      }
+      if (j == 10 && c != '\n') {
+        fprintf(stderr, "The number of chars in row more than 10.");
+        return EXIT_FAILURE;
+      }
+    }
+  }
+
+  for (int i = 0; i < 10; i++) {
+    for (int j = 9; j >= 0; j--) {
+      fprintf(stdout, "%c", input[j][i]);
+    }
+    fprintf(stdout, "\n");
+  }
+  return EXIT_SUCCESS;
+}
