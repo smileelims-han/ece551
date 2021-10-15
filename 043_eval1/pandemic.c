f54ee55b067d3bbf15f5fde71e6cf7ba83f0c46b
xh114 <xh114@duke.edu>
2021-10-15 14:59:23 -0400

    gml8 copying 943 files to 043 for extension grading

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -4,70 +4,159 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* get_location is move the input pointer to
+the index where meet the first desired symbol
+in the array the pointer point to*/
+char * get_location(char * calc, char symbol) {
+  for (size_t i = 0; *calc != symbol; i++) {
+    calc++;
+  }
+  return calc;
+}
+
 country_t parseLine(char * line) {
-  /* check if *line is empty string */
   if (line == NULL) {
-    printf("Input is empty.");
+    fprintf(stderr, "Input is empty.");
     exit(EXIT_FAILURE);
   }
 
-  /* if not empty string,find the first comma */
-  /* set up the parameter */
+  /* find the first comma */
+  /* set up two pointers, one will move in
+  the array to until meet the desired symbol;
+  one will set at the begining for reference.
+  The difference between the two pointers is
+  the length of name or population.*/
   country_t ans;
-  const char *calc, *ref;
-  size_t name_length, pop_length;
+  size_t name_length;
+  char *calc, *ref;
   calc = line;
   ref = line;
 
-  /* check the location of comma, at the same time check if there has incorrect null terminal inside name */
-  for (int i = 0; *calc != ','; i++) {
-    if (*calc != '\0') {
-      printf("There is incorrect type in country's name.");
-      exit(EXIT_FAILURE);
-    }
-    else {
-      calc++;
-    }
-  }
+  /* Get the location of comma and the length of name  */
+  calc = get_location(calc, ',');
   name_length = calc - ref;
 
-  /* copy the chars in line before comma into ans.name */
+  /* copy the chars in sting before comma into ans.name */
   strncpy(ans.name, line, name_length);
   ans.name[name_length] = '\0';
 
-  /* Then check the chars after comma, are they all numbers */
+  /* Then start work for then population */
+  /* move the pointers to the first chars after comma */
   calc++;
   ref = calc;
+  ans.population = 0ull;
+  size_t pop_length;
 
-  for (int j = 0; *calc != '\0'; j++) {
-    if (*calc != ',') {
-      printf("There is a comma inside pupolation.");
-      exit(EXIT_FAILURE);
-    }
-    else if (*calc > 57 || *calc < 48) {
-      printf("The population should be all numbers.");
-      exit(EXIT_FAILURE);
-    }
-    calc++;
+  /* check if the input population is empty */
+  if (*calc == '\n') {
+    fprintf(stderr, "These is no input for population.\n");
+    exit(EXIT_FAILURE);
   }
+
+  /* get the length of the whold population input */
+  calc = get_location(calc, '\0');
   pop_length = calc - ref;
 
-  ans.name[0] = '\0';
-  ans.population = 0;
+  /* copy the population part into a new string */
+  char * pop[pop_length];
+  for (int k = 0; *ref != *calc; k++) {
+    pop[k] = ref;
+    ref++;
+  }
+
+  /* using strtoll to transfer the chars into numbers */
+  ans.population = strtoll(*pop, NULL, 10);
+  if (ans.population == 0ull) {
+    fprintf(stderr, "The input format for population is incorrect.\n");
+    exit(EXIT_FAILURE);
+  }
   return ans;
 }
 
 void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
-  //WRITE ME
+  /* return error if the input data is empty
+     or the inputs days less than 7 days (so we cannot calculate)*/
+  if (data == NULL) {
+    fprintf(stderr, "There is an emtpy input.\n");
+    exit(EXIT_FAILURE);
+  }
+  if (n_days < 7) {
+    fprintf(stderr, "There is not enough days as input.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  size_t avg_length = n_days - 6;
+  unsigned * ptr = data;
+  unsigned * ref = data;
+
+  /* calculate the sum of 7 days(from the start date),
+     then divided into 7*/
+  for (size_t i = 0; i < avg_length; i++) {
+    double sum = 0;
+    for (size_t j = 0; j < 7; j++) {
+      sum = sum + *ptr;
+      ptr++;
+    }
+    avg[i] = sum / 7.0;
+    ptr = ref + i + 1; /*every time after calculate 7 days,
+	          	the pointer goes back to the one next to index i*/
+  }
 }
 
 void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) {
-  //WRITE ME
+  /* check if the input data is empty*/
+  if (data == NULL) {
+    fprintf(stderr, "There is an empty input.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  unsigned sum = 0;
+  for (size_t i = 0; i < n_days; i++) {
+    sum += *(data + i);
+    *(cum + i) = (sum * 1.0 / pop) * 100000;
+  }
+}
+
+/* function findMax for step 4 */
+size_t findMax(unsigned * data, size_t n_days) {
+  size_t maxnum_loc = 0;
+  unsigned maxnum;
+  maxnum = *data;
+
+  /* compare the max with each number and
+  update the max, stored the index of maxnumber 
+  in the giving string */
+  for (size_t i = 0; i < n_days; i++) {
+    if (maxnum < *(data + i)) {
+      maxnum = *(data + i);
+      maxnum_loc = i;
+    }
+  }
+  return maxnum_loc;
 }
 
 void printCountryWithMax(country_t * countries,
                          size_t n_countries,
                          unsigned ** data,
                          size_t n_days) {
-  //WRITE ME
+  /*check if the inputs data are empty */
+  if (countries == NULL || data == NULL) {
+    fprintf(stderr, "There is an emtpy input.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  /*first, find the maxnumber of each country in
+  given days, stored it into a new array which ordered
+  based on the country sequence in the giving array*/
+  unsigned maxPer_con[n_countries];
+  for (size_t i = 0; i < n_countries; i++) {
+    size_t max_loc = findMax(*(data + i), n_days);
+    maxPer_con[i] = *(data[i] + max_loc);
+  }
+
+  /*Then, find the maxnumber of all countries */
+  size_t max_country = findMax(maxPer_con, n_countries);
+  printf("%s has the most daily cases with %u\n",
+         (countries + max_country)->name,
+         *(maxPer_con + max_country));
 }
--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -14,8 +14,63 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+char ** read_sort(FILE * file) {
+  char ** string = NULL;
+  char * lines = NULL;
+  size_t siz = 0;
+  size_t index = 0;
+  while (getline(&lines, &siz, file)) {
+    string = realloc(string, (index + 1) * sizeof(*string));
+    string[index] = lines;
+    lines = NULL;
+    index++;
+  }
+  free(lines);
+  sortData(string, index);
+  return string;
+}
+
+void pr_result(char ** sorted, size_t size_sorted) {
+  for (size_t i = 0; i < size_sorted; i++) {
+    printf("%s", sorted[i]);
+    free(sorted[i]);
+  }
+  free(sorted);
+}
+
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
-
+  size_t sz = 0;
+  char ** file_string = NULL;
+  FILE * f;
+  if (argc <= 1) {
+    f = stdin;
+    if (f == NULL) {
+      fprintf(stderr, "There is no input in stdin.\n");
+      return EXIT_FAILURE;
+    }
+    file_string = read_sort(f);
+    pr_result(file_string, sz);
+    if (fclose(f) != 0) {
+      fprintf(stderr, "The file cannot close.\n");
+      return EXIT_FAILURE;
+    }
+  }
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      sz = 0;
+      f = fopen(argv[i], "r");
+      if (f == NULL) {
+        fprintf(stderr, "This is no input in the input files.\n");
+        return EXIT_FAILURE;
+      }
+      file_string = read_sort(f);
+      pr_result(file_string, sz);
+      if (fclose(f) != 0) {
+        fprintf(stderr, "The file cannot close.\n");
+        return EXIT_FAILURE;
+      }
+    }
+  }
   return EXIT_SUCCESS;
 }
