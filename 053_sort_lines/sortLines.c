89055a30eca2e9044fcb55ec4eba2e43f5bc0323
xh114 <xh114@duke.edu>
2021-10-16 20:52:02 -0400

    m

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -14,7 +14,7 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
-void read_sort_print(FILE * file) {
+void read_sort_print(void * file) {
   size_t sz;
   size_t count = 0;
   char ** arr_string = NULL;
@@ -34,6 +34,21 @@ void read_sort_print(FILE * file) {
   free(arr_string);
 }
 
+void read_file(int argc, char ** argv) {
+  for (int i = 1; i < argc - 1; i++) {
+    FILE * f = fopen(argv[i], "r");
+    if (f == NULL) {
+      fprintf(stderr, "The input file is empty.\n");
+      exit(EXIT_FAILURE);
+    }
+    read_sort_print(f);
+    if (fclose(f) != 0) {
+      fprintf(stderr, "The file cannot close.\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+}
+
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
   FILE * f;
@@ -46,18 +61,7 @@ int main(int argc, char ** argv) {
     read_sort_print(f);
   }
   if (argc > 1) {
-    for (int i = 1; i < argc - 1; i++) {
-      f = fopen(argv[i], "r");
-      if (f == NULL) {
-        fprintf(stderr, "The input file is empty.\n");
-        return EXIT_FAILURE;
-      }
-      read_sort_print(f);
-      if (fclose(f) != 0) {
-        fprintf(stderr, "The file cannot close.\n");
-        return EXIT_FAILURE;
-      }
-    }
+    read_file(argc, argv);
   }
   return EXIT_SUCCESS;
 }
