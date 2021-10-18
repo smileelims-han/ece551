2c158c8397fac58e5f44a199e6984758d6c6bfc6
xh114 <xh114@duke.edu>
2021-10-18 00:15:12 -0400

    m

--- 058_counts/counts.c
+++ 058_counts/counts.c
@@ -1,17 +1,56 @@
+#include "counts.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "counts.h"
 counts_t * createCounts(void) {
   //WRITE ME
+  counts_t * counts = malloc(sizeof(*counts));
+  counts->counts = NULL;
+  counts->num_counts = 0;
+  counts->num_unknown = 0;
+  return counts;
 }
+
+void add_value(counts_t * c, char * name) {
+  c->num_counts++;
+  c->counts = realloc(c->counts, c->num_counts * sizeof(*c->counts));
+  c->counts[c->num_counts - 1] = malloc(sizeof(**c->counts));
+  c->counts[c->num_counts - 1]->num_value = 1;
+  c->counts[c->num_counts - 1]->value = name;
+}
+
 void addCount(counts_t * c, const char * name) {
   //WRITE ME
+  if (name == NULL) {
+    c->num_unknown++;
+  }
+  else {
+    for (size_t i = 0; i < c->num_counts; i++) {
+      if (strcmp(c->counts[i]->value, name) == 0) {
+        c->counts[i]->num_value++;
+        exit(EXIT_SUCCESS);
+      }
+    }
+    char * value = strdup(name);
+    add_value(c, value);
+  }
 }
 void printCounts(counts_t * c, FILE * outFile) {
   //WRITE ME
+  for (size_t i = 0; i < c->num_counts; i++) {
+    fprintf(outFile, "%s: %lu\n", c->counts[i]->value, c->counts[i]->num_value);
+  }
+  if (c->num_unknown != 0) {
+    fprintf(outFile, "<unknown>: %lu\n", c->num_unknown);
+  }
 }
 
 void freeCounts(counts_t * c) {
   //WRITE ME
+  for (size_t i = 0; i < c->num_counts; i++) {
+    free(c->counts[i]);
+  }
+  free(c->counts);
+  free(c);
 }
--- 058_counts/counts.h
+++ 058_counts/counts.h
@@ -1,13 +1,19 @@
 #ifndef __COUNTS_H__
 #define __COUNTS_H__
+#include <stdio.h>
+
 struct _one_count_t {
   //DEFINE ME
-
+  char * value;
+  size_t num_value;
 };
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
   //DEFINE ME
+  one_count_t ** counts;
+  size_t num_counts;
+  size_t num_unknown;
 };
 typedef struct _counts_t counts_t;
 
