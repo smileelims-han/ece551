ae5d18cf5bd67786218a958bf05236a07ecfc8ee
xh114 <xh114@duke.edu>
2021-11-07 01:53:57 -0500

    m

--- 063_read_refs1/refs.cpp
+++ 063_read_refs1/refs.cpp
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 int & f(int * ptr, int & r) {
-  r ++;
+  r++;
   ptr[3] += r;
   return ptr[1];
 }
 
 int main(void) {
-  int data[] = {6, 7, 8, 9,10};
+  int data[] = {6, 7, 8, 9, 10};
   int & x = data[1];
   x = data[2];
   int * p = &x;
