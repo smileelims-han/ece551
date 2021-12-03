10a57ba9f5d89331756856253d15ea4c821e229f
xh114 <xh114@duke.edu>
2021-12-03 14:32:09 -0500

    m

--- 092_lls/ll.hpp
+++ 092_lls/ll.hpp
@@ -9,6 +9,11 @@
 //YOUR CODE GOES HERE
 using namespace std;
 
+class Error : public exception {
+ public:
+  virtual const char * what() const throw() { return "The input index is incorrect\n"; }
+};
+
 template<typename T>
 class LinkedList {
  private:
@@ -48,83 +53,75 @@ class LinkedList {
     }
     size++;
   }
+
   //bool remove
   bool remove(const T & data) {
     if (head == NULL) {
       return false;
     }
     Node * current = head;
-    while (current != NULL && current->data != data) {
-      current = current->next;
-    }
-    if (current == NULL) {
-      return false;
-    }
-    else {
-      if (head == tail) {
-        delete current;
-        head = NULL;
-        tail = NULL;
-        size = 0;
-        return true;
-      }
-      if (current == head) {
-        head = current->next;
-        current->next->prev = NULL;
-        delete current;
-        size--;
-        return true;
-      }
-      if (current == tail) {
-        tail = current->prev;
-        current->prev->next = NULL;
-        delete current;
-        size--;
-        return true;
+    while (current != NULL) {
+      if (current->data != data) {
+        current = current->next;
       }
       else {
-        current->prev->next = current->next;
-        current->next->prev = current->prev;
-        delete current;
-        size--;
-        return true;
+        if (head == tail) {
+          delete current;
+          head = NULL;
+          tail = NULL;
+          size = 0;
+          return true;
+        }
+        if (current == head) {
+          head = current->next;
+          current->next->prev = NULL;
+          delete current;
+          size--;
+          return true;
+        }
+        if (current == tail) {
+          tail = current->prev;
+          current->prev->next = NULL;
+          delete current;
+          size--;
+          return true;
+        }
+        else {
+          current->prev->next = current->next;
+          current->next->prev = current->prev;
+          delete current;
+          size--;
+          return true;
+        }
       }
     }
+    return false;
   }
+
   //index []
   T & operator[](int index) {
-    try {
-      if (index > size || index < 0) {
-        throw exception();
-      }
-      Node * current = head;
-      for (int i = 0; i < index; i++) {
-        current = current->next;
-      }
-      return current->data;
+    Node * current = head;
+    if (index < 0 || index > size) {
+      throw Error();
     }
-    catch (exception & e) {
-      cout << " the input index is incorret " << e.what() << "\n";
-      exit(EXIT_FAILURE);
+    for (int i = 0; i < index; i++) {
+      current = current->next;
     }
+    return current->data;
   }
+
   //index [] const
-  T & operator[](int index) const {
-    try {
-      if (index > size || index < 0) {
-        throw exception();
-      }
-      Node * current = head;
-      for (int i = 0; i < index; i++) {
-        current = current->next;
-      }
-      return current->data;
+  const T & operator[](int index) const {
+    Node * current = head;
+    if (index < 0 || index > size) {
+      throw Error();
     }
-    catch (exception & e) {
-      cout << " the input index is incorret " << e.what() << "\n";
-      exit(EXIT_FAILURE);
+    for (int i = 0; i < index; i++) {
+      current = current->next;
     }
+    return current->data;
   }
+
   //find
   int find(const T & item) {
     if (head == NULL) {
@@ -168,14 +165,16 @@ class LinkedList {
     }
     return *this;
   }
+
   //copy
-  LinkedList(const LinkedList & rhs) : head(NULL), tail(NULL), size(rhs.getSize()) {
+  LinkedList(const LinkedList & rhs) : head(NULL), tail(NULL), size(rhs.size) {
     Node * current = rhs.head;
     while (current != NULL) {
       addBack(current->data);
       current = current->next;
     }
   }
+
   //getSize
   int getSize() const { return size; }
 
